d6bf20fc0e961b486daa6f91955b8cf37bb13979
Mike Guillory <webdevmike101@gmail.com>
2023-06-01 15:24:31 -0500

    about page text

--- components/views/About.js
+++ components/views/About.js
@@ -4,6 +4,9 @@ export default (state) => html`
 <main>
   <h2 class="pageHeading" >${state.pageHeading}</h2>
   <div id="aboutDiv">
+    <p>This Bill Tracker is designed to help you get a quick, big-picture look at your bills and income situation from week to week.</p>
+    <p>Once you’ve added your bill, income and payment sources into the Data page, as you enter each payday into the app, it will
+      create a Bill Payment Schedule and display how much of your pay is left once the bills for that period have been paid.</p>
   </div>
 </main>
 `;
